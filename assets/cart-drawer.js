127efbe4fc2bd0d8c71cf532e8c551acf016db86
mazal-tov-traders <d90.german@gmail.com>
2023-06-22 11:47:39 +0300

    cart-drawe

--- assets/cart-drawer.js
+++ assets/cart-drawer.js
@@ -1,126 +1,113 @@
-class CartDrawer extends HTMLElement {
-  constructor() {
-    super();
-
-    this.addEventListener('keyup', (evt) => evt.code === 'Escape' && this.close());
-    this.querySelector('#CartDrawer-Overlay').addEventListener('click', this.close.bind(this));
-    this.setHeaderCartIconAccessibility();
-  }
-
-  setHeaderCartIconAccessibility() {
-    const cartLink = document.querySelector('#cart-icon-bubble');
-    cartLink.setAttribute('role', 'button');
-    cartLink.setAttribute('aria-haspopup', 'dialog');
-    cartLink.addEventListener('click', (event) => {
-      event.preventDefault();
-      this.open(cartLink)
-    });
-    cartLink.addEventListener('keydown', (event) => {
-      if (event.code.toUpperCase() === 'SPACE') {
-        event.preventDefault();
-        this.open(cartLink);
-      }
-    });
-  }
-
-  open(triggeredBy) {
-    if (triggeredBy) this.setActiveElement(triggeredBy);
-    const cartDrawerNote = this.querySelector('[id^="Details-"] summary');
-    if (cartDrawerNote && !cartDrawerNote.hasAttribute('role')) this.setSummaryAccessibility(cartDrawerNote);
-    // here the animation doesn't seem to always get triggered. A timeout seem to help
-    setTimeout(() => {this.classList.add('animate', 'active')});
-
-    this.addEventListener('transitionend', () => {
-      const containerToTrapFocusOn = this.classList.contains('is-empty') ? this.querySelector('.drawer__inner-empty') : document.getElementById('CartDrawer');
-      const focusElement = this.querySelector('.drawer__inner') || this.querySelector('.drawer__close');
-      trapFocus(containerToTrapFocusOn, focusElement);
-    }, { once: true });
-
-    document.body.classList.add('overflow-hidden');
-  }
-
-  close() {
-    this.classList.remove('active');
-    removeTrapFocus(this.activeElement);
-    document.body.classList.remove('overflow-hidden');
-  }
-
-  setSummaryAccessibility(cartDrawerNote) {
-    cartDrawerNote.setAttribute('role', 'button');
-    cartDrawerNote.setAttribute('aria-expanded', 'false');
-
-    if(cartDrawerNote.nextElementSibling.getAttribute('id')) {
-      cartDrawerNote.setAttribute('aria-controls', cartDrawerNote.nextElementSibling.id);
-    }
-
-    cartDrawerNote.addEventListener('click', (event) => {
-      event.currentTarget.setAttribute('aria-expanded', !event.currentTarget.closest('details').hasAttribute('open'));
-    });
-
-    cartDrawerNote.parentElement.addEventListener('keyup', onKeyUpEscape);
-  }
-
-  renderContents(parsedState) {
-    this.querySelector('.drawer__inner').classList.contains('is-empty') && this.querySelector('.drawer__inner').classList.remove('is-empty');
-    this.productId = parsedState.id;
-    this.getSectionsToRender().forEach((section => {
-      const sectionElement = section.selector ? document.querySelector(section.selector) : document.getElementById(section.id);
-      sectionElement.innerHTML =
-          this.getSectionInnerHTML(parsedState.sections[section.id], section.selector);
-    }));
-
-    setTimeout(() => {
-      this.querySelector('#CartDrawer-Overlay').addEventListener('click', this.close.bind(this));
-      this.open();
-    });
-  }
-
-  getSectionInnerHTML(html, selector = '.shopify-section') {
-    return new DOMParser()
-      .parseFromString(html, 'text/html')
-      .querySelector(selector).innerHTML;
-  }
-
-  getSectionsToRender() {
-    return [
-      {
-        id: 'cart-drawer',
-        selector: '#CartDrawer'
-      },
-      {
-        id: 'cart-icon-bubble'
-      }
-    ];
-  }
-
-  getSectionDOM(html, selector = '.shopify-section') {
-    return new DOMParser()
-      .parseFromString(html, 'text/html')
-      .querySelector(selector);
-  }
-
-  setActiveElement(element) {
-    this.activeElement = element;
-  }
+function openCartDrawer() {
+  document.querySelector(".cart-drawer").classList.add("cart-drawer--active");
 }
 
-customElements.define('cart-drawer', CartDrawer);
-
-class CartDrawerItems extends CartItems {
-  getSectionsToRender() {
-    return [
-      {
-        id: 'CartDrawer',
-        section: 'cart-drawer',
-        selector: '.drawer__inner'
-      },
-      {
-        id: 'cart-icon-bubble',
-        section: 'cart-icon-bubble',
-        selector: '.shopify-section'
-      }
-    ];
-  }
+function closeCartDrawer() {
+  document
+    .querySelector(".cart-drawer")
+    .classList.remove("cart-drawer--active");
 }
 
-customElements.define('cart-drawer-items', CartDrawerItems);
+function updateCartItemCounts(count) {
+  document.querySelectorAll(".cart-count").forEach((el) => {
+    el.textContent = count;
+  });
+}
+
+async function updateCartDrawer() {
+  const res = await fetch("/?section_id=cart-drawer");
+  const text = await res.text();
+  const html = document.createElement("div");
+  html.innerHTML = text;
+
+  const newBox = html.querySelector(".cart-drawer").innerHTML;
+
+  document.querySelector(".cart-drawer").innerHTML = newBox;
+
+  addCartDrawerListeners();
+}
+
+function addCartDrawerListeners() {
+  // Update quantities
+  document
+    .querySelectorAll(".cart-drawer-quantity-selector button")
+    .forEach((button) => {
+      button.addEventListener("click", async () => {
+        // Get line item key
+        const rootItem =
+          button.parentElement.parentElement.parentElement.parentElement
+            .parentElement;
+        const key = rootItem.getAttribute("data-line-item-key");
+
+        // Get new quantity
+        const currentQuantity = Number(
+          button.parentElement.querySelector("input").value
+        );
+        const isUp = button.classList.contains(
+          "cart-drawer-quantity-selector-plus"
+        );
+        const newQuantity = isUp ? currentQuantity + 1 : currentQuantity - 1;
+
+        // Ajax update\
+        const res = await fetch("/cart/update.js", {
+          method: "post",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ updates: { [key]: newQuantity } }),
+        });
+        const cart = await res.json();
+
+        updateCartItemCounts(cart.item_count);
+
+        // Update cart
+        updateCartDrawer();
+      });
+    });
+
+  document.querySelector(".cart-drawer-box").addEventListener("click", (e) => {
+    e.stopPropagation();
+  });
+
+  document
+    .querySelectorAll(".cart-drawer-header-right-close, .cart-drawer")
+    .forEach((el) => {
+      el.addEventListener("click", () => {
+        console.log("closing drawer");
+        closeCartDrawer();
+      });
+    });
+}
+
+addCartDrawerListeners();
+
+document.querySelectorAll('form[action="/cart/add"]').forEach((form) => {
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    // Submit form with ajax
+    await fetch("/cart/add", {
+      method: "post",
+      body: new FormData(form),
+    });
+
+    // Get cart count
+    const res = await fetch("/cart.js");
+    const cart = await res.json();
+    updateCartItemCounts(cart.item_count);
+
+    // Update cart
+    await updateCartDrawer();
+
+    // Open cart drawer
+    openCartDrawer();
+  });
+});
+
+document.querySelectorAll('a[href="/cart"]').forEach((a) => {
+  a.addEventListener("click", (e) => {
+    e.preventDefault();
+    openCartDrawer();
+  });
+});
